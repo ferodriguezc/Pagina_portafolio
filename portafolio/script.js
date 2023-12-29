e9bac41aa95d5bf31f66958e2180f8b1d52b0934
ferodriguezc <115609396+ferodriguezc@users.noreply.github.com>
2023-12-28 23:16:10 -0300

    Delete portafolio directory

--- portafolio/script.js
+++
@@ -1,8 +0,0 @@
-function cambiarDiv(seccion) {
-    var secciones = ['Sobre_Mi', 'Conocimientos', 'proyectos', 'conctato'];
-
-    secciones.forEach(function (item) {
-        var div = document.getElementById(item);
-        div.style.display = item === seccion ? 'block' : 'none';
-    });
-}
